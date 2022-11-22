c0d4f280e2ed414caaeb43a14c5877d57a4f8c1f
suhye0n <contact.suhyeon@gmail.com>
2022-11-22 22:51:38 +0900

    게시글 작성폼 생성

--- src/main/java/com/board/board/controller/BoardController.java
+++ src/main/java/com/board/board/controller/BoardController.java
@@ -2,14 +2,12 @@ package com.board.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BoardController {
 
-    @GetMapping("/")
-    @ResponseBody
-    public String main() {
-        return "Hello";
+    @GetMapping("/board/write") //localhost:8090/board/write
+    public String boardWriteForm() {
+        return "boardwrite";
     }
 }
